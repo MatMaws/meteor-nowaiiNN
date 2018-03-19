78d4559b27fbf8deea9ed9a5aa502dc24ad522a1
Tanguy DEPOMMIER <tanguy.depommier@etudiant.univ-lille1.fr>
2018-03-19 10:20:39 +0100

    End step 1

--- imports/startup/client/useraccounts-configuration.js
+++ imports/startup/client/useraccounts-configuration.js
@@ -1,3 +1,5 @@
 import {Accounts} from 'meteor/accounts-base';
 
-// TODO : Injecter le code pour l'authent
+Accounts.ui.config({
+    passwordSignupFields: 'USERNAME_ONLY', // un pseudo au lieu d'un email pour l'authentification
+});
--- imports/ui/components/navbar/index.js
+++ imports/ui/components/navbar/index.js
@@ -3,4 +3,9 @@ import './navbar.css';
 
 import { Template } from 'meteor/templating';
 
-// TODO: Ajouter les helpers
+Template.navbar.helpers({
+    dollarWallet() {
+        return 9876;
+    },
+    currency: 'dollars',
+});
